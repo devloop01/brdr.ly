cb6742917faabe096f23333bb32a9fe7690e25cd
Sikriti Dakua <destrodev123@gmail.com>
2023-09-17 02:13:33 +0530

    feat: add icon components

---
+++ src/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as ClipboardCopyIcon } from './clipboard-copy.svelte'
+export { default as RefreshIcon } from './refresh.svelte'
+export { default as ShuffleIcon } from './shuffle.svelte'
