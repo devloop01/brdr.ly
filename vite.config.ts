6fb23c250bc482942ee107af718e698025e7a403
Sikriti Dakua <destrodev123@gmail.com>
2023-09-17 02:43:11 +0530

    fix: reolved `paths`

--- vite.config.ts
+++ vite.config.ts
@@ -26,8 +26,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': 'src/',
-      '@/*': 'src/*',
+      '@': path.resolve(__dirname, 'src'),
+      '@/*': path.resolve(__dirname, 'src/*'),
     },
   },
 })
