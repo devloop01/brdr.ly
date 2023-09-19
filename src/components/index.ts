b5748480401cbb15b666bc830fcff6b8af612139
Sikriti Dakua <destrodev123@gmail.com>
2023-09-20 01:17:52 +0530

    add: `<ToggleTheme />` component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
-export { default as Button } from './button.svelte';
 export * from './generator';
+export { default as Button } from './button.svelte';
+export { default as ThemeToggle } from './theme-toggle.svelte';
 
---
+++ src/components/theme-toggle.svelte
@@ -0,0 +1,18 @@
+<script lans="ts">
+	import { ctx } from '~/context';
+	import { Button } from '.';
+	import { MoonIcon, SunIcon } from '~/icons';
+
+	const {
+		states: { theme },
+		helpers: { toggleTheme }
+	} = ctx.popup.get().theme;
+</script>
+
+<Button size="square" on:click={toggleTheme}>
+	{#if theme === 'dark'}
+		<MoonIcon />
+	{:else}
+		<SunIcon />
+	{/if}
+</Button>
