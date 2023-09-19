a9bf009db3999d1ead336618b03b4930592a065f
Sikriti Dakua <destrodev123@gmail.com>
2023-09-20 01:19:35 +0530

    fix: add `toggleTheme()` function

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -2,11 +2,11 @@ import type { Theme } from '~/types';
 import { persisted } from '~/utils';
 
 export function createTheme() {
-	const { subscribe, set } = persisted<Theme>('theme', 'light');
+	const { subscribe, set, update } = persisted<Theme>('brdrly-theme', 'light');
 
 	return {
 		states: { theme: { subscribe } },
-		helpers: { setTheme: set }
+		helpers: { setTheme: set, toggleTheme: () => update((theme) => (theme === 'light' ? 'dark' : 'light')) }
 	};
 }
 
