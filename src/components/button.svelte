f08e2b0905ed6af0f08538c4ba469c92ff8b38c5
Sikriti Dakua <destrodev123@gmail.com>
2023-09-20 01:17:20 +0530

    update: button `variants` and style

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -5,7 +5,7 @@
 
 	export const buttonVariants = cva({
 		base: [
-			'relative inline-flex items-center justify-center rounded bg-white px-6 py-3 text-lg font-medium text-black ring-2 ring-inset ring-black transition-all duration-150 ease-in-out shadow-3px',
+			'relative inline-flex items-center justify-center rounded bg-white px-6 h-12 text-lg font-medium text-black ring-2 ring-inset ring-black transition-all duration-150 ease-in-out shadow-3px',
 			'hover:translate-x-[1.5px] hover:translate-y-[1.5px] hover:shadow-1.5px',
 			'active:translate-x-[3px] active:translate-y-[3px] active:shadow-0px',
 			'focus:outline-none focus:translate-x-[1.5px] focus:translate-y-[1.5px] focus:shadow-1.5px',
@@ -17,10 +17,15 @@
 				primary: 'bg-green-400',
 				secondary: 'bg-yellow-300',
 				destructive: 'bg-red-400'
+			},
+			size: {
+				default: '',
+				square: 'h-auto p-3'
 			}
 		},
 		defaultVariants: {
-			variant: 'default'
+			variant: 'default',
+			size: 'default'
 		}
 	});
 
@@ -30,13 +35,15 @@
 <script lang="ts">
 	interface $$Props extends HTMLButtonAttributes {
 		variant?: ButtonVariants['variant'];
+		size?: ButtonVariants['size'];
 	}
 
 	export let variant: $$Props['variant'] = 'default';
+	export let size: $$Props['size'] = 'default';
 	let className: $$Props['class'] = undefined;
 	export { className as class };
 </script>
 
-<button type="button" class={buttonVariants({ variant, className })} on:click {...$$restProps}>
+<button type="button" class={buttonVariants({ variant, size, className })} on:click {...$$restProps}>
 	<slot />
 </button>
